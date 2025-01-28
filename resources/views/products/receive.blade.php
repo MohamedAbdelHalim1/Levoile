8669fc672222e8412df81d1dd06ef0d0546cb8a0
Mohamed Abdelhalim <mohamedabdelhalim2804@gmail.com>
2025-01-28 19:41:08 +0200

    fix model role permission 14

--- resources/views/products/receive.blade.php
+++ resources/views/products/receive.blade.php
@@ -91,7 +91,7 @@
 
                 <div>
                     <div class="row">
-                        <div class="col-md-12 d-flex">
+                        <div class="col-md-12 d-flex justify-content-arround">
                             @if ($product->photo)
                                 <img src="{{ asset($product->photo) }}" alt="Product Image" class="product-image">
                             @endif
