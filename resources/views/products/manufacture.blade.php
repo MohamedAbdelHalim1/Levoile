a285b3cb2dc2e647272df95f6d0b9441b888f62e
Mohamed Abdelhalim <mohamedabdelhalim2804@gmail.com>
2025-03-06 21:15:31 +0200

    separate multiple material15

--- resources/views/products/manufacture.blade.php
+++ resources/views/products/manufacture.blade.php
@@ -2,6 +2,7 @@
 
 @section('content')
 
+@dd($materials , $factories);
     <div class="p-2">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
             <div class="p-8 bg-white shadow sm:rounded-lg border border-gray-200">
