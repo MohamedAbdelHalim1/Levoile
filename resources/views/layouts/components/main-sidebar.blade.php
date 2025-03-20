181e303aaa7c4a73bd430ec53b6ce04b918abfe3
Mohamed Abdelhalim <mohamedabdelhalim2804@gmail.com>
2025-03-20 21:19:13 +0200

    shooting3

--- resources/views/layouts/components/main-sidebar.blade.php
+++ resources/views/layouts/components/main-sidebar.blade.php
@@ -212,7 +212,7 @@
                 </li>
                 <li class="slide">
                     <a class="side-menu__item" data-bs-toggle="slide" href="javascript:void(0)"><i
-                            class="side-menu__icon fe fe-user"></i><span class="side-menu__label">المستخدمين</span><i
+                            class="side-menu__icon fe fe-camera"></i><span class="side-menu__label">التصوير</span><i
                             class="angle fe fe-chevron-right"></i></a>
                     <ul class="slide-menu">
                         <li class="panel sidetab-menu">
