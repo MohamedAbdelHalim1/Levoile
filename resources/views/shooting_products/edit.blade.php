99280579c68f084445ac5293d98a477b2b62b5fc
Mohamed Abdelhalim <mohamedabdelhalim2804@gmail.com>
2025-03-22 21:00:23 +0200

    shooting28

--- resources/views/shooting_products/edit.blade.php
+++ resources/views/shooting_products/edit.blade.php
@@ -6,16 +6,16 @@
             <div class="bg-white shadow sm:rounded-lg p-4">
                 <h2 class="text-lg font-bold mb-4">إضافة منتج جديد</h2>
 
-                <form action="{{ route('shooting-products.update') }}" method="POST">
+                <form action="{{ route('shooting-products.update' , $product->id) }}" method="POST">
                     @csrf
                     <div class="mb-3">
                         <label class="form-label">اسم المنتج</label>
-                        <input type="text" name="name" class="form-control" value="{{ $shootingProduct->name }}" required>
+                        <input type="text" name="name" class="form-control" value="{{ $product->name }}" required>
                     </div>
 
                     <div class="mb-3">
                         <label class="form-label">عدد الألوان</label>
-                        <input type="number" name="number_of_colors" value="{{ $shootingProduct->number_of_colors }}" class="form-control">
+                        <input type="number" name="number_of_colors" value="{{ $product->number_of_colors }}" class="form-control">
                     </div>
 
                     <button type="submit" class="btn btn-primary">إضافة المنتج</button>
