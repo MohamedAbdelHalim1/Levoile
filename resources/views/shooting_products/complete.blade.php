6969cc55fd313f895aa82870d7cfbdb42688b5b6
Mohamed Abdelhalim <mohamedabdelhalim2804@gmail.com>
2025-04-06 14:49:27 +0200

    complete data4

--- resources/views/shooting_products/complete.blade.php
+++ resources/views/shooting_products/complete.blade.php
@@ -22,26 +22,30 @@
                     <hr>
                     <h5>تفاصيل الألوان</h5>
 
-                    @for ($i = 1; $i <= $product->number_of_colors; $i++)
-                        <div class="border p-3 mb-3 rounded bg-light">
-                            <h6>لون {{ $i }}</h6>
-                            <div class="mb-2">
-                                <label>اسم اللون</label>
-                                <input type="text" name="colors[{{ $i }}][name]" class="form-control"
-                                    required>
+                    <div class="row">
+                        @for ($i = 1; $i <= $product->number_of_colors; $i++)
+                            <div class="col-md-4">
+                                <div class="border p-3 mb-3 rounded bg-light">
+                                    <h6>لون {{ $i }}</h6>
+                                    <div class="mb-2">
+                                        <label>اسم اللون</label>
+                                        <input type="text" name="colors[{{ $i }}][name]" class="form-control"
+                                            required>
+                                    </div>
+                                    <div class="mb-2">
+                                        <label>الكود</label>
+                                        <input type="text" name="colors[{{ $i }}][code]" class="form-control"
+                                            required>
+                                    </div>
+                                    <div class="mb-2">
+                                        <label>الصورة</label>
+                                        <input type="file" name="colors[{{ $i }}][image]" class="form-control"
+                                            accept="image/*" required>
+                                    </div>
+                                </div>
                             </div>
-                            <div class="mb-2">
-                                <label>الكود</label>
-                                <input type="text" name="colors[{{ $i }}][code]" class="form-control"
-                                    required>
-                            </div>
-                            <div class="mb-2">
-                                <label>الصورة</label>
-                                <input type="file" name="colors[{{ $i }}][image]" class="form-control"
-                                    accept="image/*" required>
-                            </div>
-                        </div>
-                    @endfor
+                        @endfor
+                    </div>
 
                     <button type="submit" class="btn btn-primary">حفظ البيانات</button>
                     <a href="{{ route('shooting-products.index') }}" class="btn btn-secondary">رجوع</a>
