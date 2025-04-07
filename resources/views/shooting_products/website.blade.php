182ad7880c87130f97275a714beb0638f1314020
Mohamed Abdelhalim <mohamedabdelhalim2804@gmail.com>
2025-04-07 13:56:51 +0200

    website admin3

--- resources/views/shooting_products/website.blade.php
+++ resources/views/shooting_products/website.blade.php
@@ -6,7 +6,7 @@
             <div class="bg-white shadow sm:rounded-lg p-4">
                 <h4>منتجات مسؤول الموقع</h4>
 
-                <table class="table table-bordered">
+                <table id="file-datatable" class="table table-bordered text-nowrap key-buttons border-bottom">
                     <thead>
                         <tr>
                             <th>اسم المنتج</th>
@@ -72,6 +72,26 @@
 @endsection
 
 @section('scripts')
+    <!-- SELECT2 JS -->
+    <script src="{{ asset('build/assets/plugins/select2/select2.full.min.js') }}"></script>
+    @vite('resources/assets/js/select2.js')
+
+    <!-- DATA TABLE JS -->
+    <script src="{{ asset('build/assets/plugins/datatable/js/jquery.dataTables.min.js') }}"></script>
+    <script src="{{ asset('build/assets/plugins/datatable/js/dataTables.bootstrap5.js') }}"></script>
+    <script src="{{ asset('build/assets/plugins/datatable/js/dataTables.buttons.min.js') }}"></script>
+    <script src="{{ asset('build/assets/plugins/datatable/js/buttons.bootstrap5.min.js') }}"></script>
+    <script src="{{ asset('build/assets/plugins/datatable/js/jszip.min.js') }}"></script>
+    <script src="{{ asset('build/assets/plugins/datatable/pdfmake/pdfmake.min.js') }}"></script>
+    <script src="{{ asset('build/assets/plugins/datatable/pdfmake/vfs_fonts.js') }}"></script>
+    <script src="{{ asset('build/assets/plugins/datatable/js/buttons.html5.min.js') }}"></script>
+    <script src="{{ asset('build/assets/plugins/datatable/js/buttons.print.min.js') }}"></script>
+    <script src="{{ asset('build/assets/plugins/datatable/js/buttons.colVis.min.js') }}"></script>
+    <script src="{{ asset('build/assets/plugins/datatable/dataTables.responsive.min.js') }}"></script>
+    <script src="{{ asset('build/assets/plugins/datatable/responsive.bootstrap5.min.js') }}"></script>
+    @vite('resources/assets/js/table-data.js')
+
+    
     <script>
         const modal = document.getElementById('confirmModal');
         modal.addEventListener('show.bs.modal', function(event) {
