088cce1a92f9e3e5d19388569877103d61bade8c
Mohamed Abdelhalim <mohamedabdelhalim2804@gmail.com>
2025-04-07 20:55:55 +0200

    website admin14

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,8 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Request;
+use Carbon\Carbon;
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -25,7 +27,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-       
+        Carbon::setLocale('ar');
     }
     
   
