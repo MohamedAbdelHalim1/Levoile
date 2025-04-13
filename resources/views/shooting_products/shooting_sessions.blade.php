b31018fdf47c325c119abb3a7b6ada115c2e7e7f
Mohamed Abdelhalim <mohamedabdelhalim2804@gmail.com>
2025-04-13 23:48:44 +0200

    shootingsession24

--- resources/views/shooting_products/shooting_sessions.blade.php
+++ resources/views/shooting_products/shooting_sessions.blade.php
@@ -1,56 +1,55 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="p-2">
-    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
-        @if (session('success'))
+    <div class="p-2">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
+            @if (session('success'))
                 <div class="alert alert-primary" role="alert">
                     <button type="button" class="btn-close" data-bs-dismiss="alert" aria-hidden="true">x</button>
                     {{ session('success') }}
                 </div>
             @endif
-        <div class="bg-white shadow sm:rounded-lg p-4">
-            <h4 class="mb-4">جلسات التصوير</h4>
+            <div class="bg-white shadow sm:rounded-lg p-4">
+                <h4 class="mb-4">جلسات التصوير</h4>
 
-            <div class="table-responsive">
-                <table id="file-datatable" class="table table-bordered text-nowrap key-buttons border-bottom">
-                    <thead class="table-light">
-                        <tr>
-                            <th>#</th>
-                            <th>جلسة التصوير</th>
-                            <th>عدد الألوان</th>
-                            <th>التحكم</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach ($sessions as $index => $session)
-                            @php
-                                $colors = \App\Models\ShootingSession::where('reference', $session->reference)
-                                            ->with('color.shootingProduct')
-                                            ->get();
-                            @endphp
+                <div class="table-responsive">
+                    <table id="file-datatable" class="table table-bordered text-nowrap key-buttons border-bottom">
+                        <thead class="table-light">
                             <tr>
-                                <td>{{ $index + 1 }}</td>
-                                <td><span class="badge bg-dark">{{ $session->reference }}</span></td>
-                                <td><span class="badge bg-primary">{{ $colors->count() }}</span></td>
-                                <td>
+                                <th>#</th>
+                                <th>جلسة التصوير</th>
+                                <th>عدد الألوان</th>
+                                <th>التحكم</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($sessions as $index => $session)
+                                @php
+                                    $colors = \App\Models\ShootingSession::where('reference', $session->reference)
+                                        ->with('color.shootingProduct')
+                                        ->get();
+                                @endphp
+                                <tr>
+                                    <td>{{ $index + 1 }}</td>
+                                    <td><span class="badge bg-dark">{{ $session->reference }}</span></td>
+                                    <td><span class="badge bg-primary">{{ $colors->count() }}</span></td>
                                     <td>
-                                        <a href="{{ route('shooting-sessions.show', $session->reference) }}" class="btn btn-info btn-sm">
+                                        <a href="{{ route('shooting-sessions.show', $session->reference) }}"
+                                            class="btn btn-info btn-sm">
                                             عرض المزيد
                                         </a>
                                     </td>
-                                    
-                                </td>
-                            </tr>
-                        @endforeach
-                    </tbody>
-                </table>
+
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+
+
             </div>
-            
-     
         </div>
     </div>
-</div>
 @endsection
 
 
@@ -73,6 +72,4 @@
     <script src="{{ asset('build/assets/plugins/datatable/dataTables.responsive.min.js') }}"></script>
     <script src="{{ asset('build/assets/plugins/datatable/responsive.bootstrap5.min.js') }}"></script>
     @vite('resources/assets/js/table-data.js')
-
-
 @endsection
