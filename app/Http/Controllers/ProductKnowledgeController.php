0658f0242e8a946f72514b195a0a651c32ae07dd
Mohamed Abdelhalim <mohamedabdelhalim2804@gmail.com>
2025-05-06 20:49:28 +0300

    product knowledge commit11

--- app/Http/Controllers/ProductKnowledgeController.php
+++ app/Http/Controllers/ProductKnowledgeController.php
@@ -31,6 +31,8 @@ class ProductKnowledgeController extends Controller
 
     public function uploadForm()
     {
+        abort(500, 'Reached the controller method'); // هل دي بتظهر؟ لو آه المشكلة في البليد
+
         return view('product_knowledge.upload');
     }
 
