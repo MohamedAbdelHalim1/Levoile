a3fac2154de6a215d670f26261e5ece49a1534bb
Mohamed Abdelhalim <mohamedabdelhalim2804@gmail.com>
2025-05-08 19:24:33 +0300

    product knowledge commits45

--- resources/views/product_knowledge/product-list.blade.php
+++ resources/views/product_knowledge/product-list.blade.php
@@ -6,14 +6,10 @@
         <div class="bg-white shadow sm:rounded-lg p-4">
             <h4>قائمة المنتجات</h4>
 
-            <form method="GET" class="mb-4 d-flex gap-2 align-items-center">
-                <input type="text" name="search" value="{{ request('search') }}" class="form-control"
-                    placeholder="ابحث باستخدام الاسم - الجملة - الكود">
-                <button type="submit" class="btn btn-primary">ابحث</button>
-            </form>
+
 
             <div class="table-responsive">
-                <table class="table table-bordered text-center align-middle">
+                <table id="file-datatable" class="table table-bordered text-nowrap key-buttons border-bottom">
                     <thead class="table-light">
                         <tr>
                             <th>صورة</th>
@@ -96,6 +92,25 @@
 @endsection
 
 @section('scripts')
+    <!-- SELECT2 JS -->
+    <script src="{{ asset('build/assets/plugins/select2/select2.full.min.js') }}"></script>
+    @vite('resources/assets/js/select2.js')
+
+    <!-- DATA TABLE JS -->
+    <script src="{{ asset('build/assets/plugins/datatable/js/jquery.dataTables.min.js') }}"></script>
+    <script src="{{ asset('build/assets/plugins/datatable/js/dataTables.bootstrap5.js') }}"></script>
+    <script src="{{ asset('build/assets/plugins/datatable/js/dataTables.buttons.min.js') }}"></script>
+    <script src="{{ asset('build/assets/plugins/datatable/js/buttons.bootstrap5.min.js') }}"></script>
+    <script src="{{ asset('build/assets/plugins/datatable/js/jszip.min.js') }}"></script>
+    <script src="{{ asset('build/assets/plugins/datatable/pdfmake/pdfmake.min.js') }}"></script>
+    <script src="{{ asset('build/assets/plugins/datatable/pdfmake/vfs_fonts.js') }}"></script>
+    <script src="{{ asset('build/assets/plugins/datatable/js/buttons.html5.min.js') }}"></script>
+    <script src="{{ asset('build/assets/plugins/datatable/js/buttons.print.min.js') }}"></script>
+    <script src="{{ asset('build/assets/plugins/datatable/js/buttons.colVis.min.js') }}"></script>
+    <script src="{{ asset('build/assets/plugins/datatable/dataTables.responsive.min.js') }}"></script>
+    <script src="{{ asset('build/assets/plugins/datatable/responsive.bootstrap5.min.js') }}"></script>
+    @vite('resources/assets/js/table-data.js')
+
 <script>
     function showColors(group) {
         const uniqueColors = [...new Set(group.map(item => item.color))];
