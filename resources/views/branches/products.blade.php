e5b27e8ed32372c8bb1840507cb8430e216248f1
Mohamed Abdelhalim <mohamedabdelhalim2804@gmail.com>
2025-05-14 18:50:23 +0300

    product knowledge order93

--- resources/views/branches/products.blade.php
+++ resources/views/branches/products.blade.php
@@ -60,15 +60,15 @@
                         </div>
                         <div class="position-absolute top-0 start-0 ms-1 mt-1">
                             <small
-                                class="fw-semibold back-ground text-white rounded-1 p-1">{{ $parent->product_code }}</small>
+                                class="fw-semibold back-ground text-white rounded-1 p-1">{{ count($group) }}
+                                colors</small>
                         </div>
                         <div class="position-absolute bottom-0 start-0 ms-1 mb-1">
                             <small
                                 class="fw-semibold back-ground text-white  rounded-1 p-1">{{ $parent->material ?? 'لا يوجد خامه' }}</small>
                         </div>
                         <div class="position-absolute bottom-0 end-0 me-1 mb-1">
-                            <small class="fw-semibold back-ground text-white rounded-1 p-1">{{ count($group) }}
-                                colors</small>
+                            <small class="fw-semibold back-ground text-white rounded-1 p-1">{{ $parent->product_code }}</small>
                         </div>
                     </div>
                     <h4>
