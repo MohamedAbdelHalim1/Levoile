811b94d150e8c7506de9d9764dec79fe47a2ad2d
Mohamed Abdelhalim <mohamedabdelhalim2804@gmail.com>
2025-05-14 18:40:31 +0300

    product knowledge order90

--- app/Http/Controllers/BranchOrderController.php
+++ app/Http/Controllers/BranchOrderController.php
@@ -205,7 +205,7 @@ class BranchOrderController extends Controller
 
         $orders = \App\Models\OpenOrder::with(['items.product'])
             ->where('user_id', $user->id)
-            ->orderByDesc('created_at')
+            ->orderByDesc('id')
             ->get();
 
         return view('branches.my-orders', compact('orders'));
