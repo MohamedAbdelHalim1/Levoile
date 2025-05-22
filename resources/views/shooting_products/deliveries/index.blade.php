96812efe8e476f414c9579352e48a4079777abc1
Mohamed Abdelhalim <mohamedabdelhalim2804@gmail.com>
2025-05-22 14:30:52 +0300

    New Updates In Shootings46

--- resources/views/shooting_products/deliveries/index.blade.php
+++ resources/views/shooting_products/deliveries/index.blade.php
@@ -9,7 +9,7 @@
                     <a href="{{ route('shooting-deliveries.upload.create') }}" class="btn btn-primary">رفع شيت جديد</a>
                 </div>
 
-                <table id="file-datatable" class="table table-bordered text-nowrap key-buttons border-bottom">
+                <table class="table table-bordered text-nowrap key-buttons border-bottom">
                     <thead>
                         <tr>
                             <th>تاريخ الرفع</th>
@@ -76,23 +76,3 @@
 @endsection
 
 
-@section('scripts')
-    <!-- SELECT2 JS -->
-    <script src="{{ asset('build/assets/plugins/select2/select2.full.min.js') }}"></script>
-    @vite('resources/assets/js/select2.js')
-
-    <!-- DATA TABLE JS -->
-    <script src="{{ asset('build/assets/plugins/datatable/js/jquery.dataTables.min.js') }}"></script>
-    <script src="{{ asset('build/assets/plugins/datatable/js/dataTables.bootstrap5.js') }}"></script>
-    <script src="{{ asset('build/assets/plugins/datatable/js/dataTables.buttons.min.js') }}"></script>
-    <script src="{{ asset('build/assets/plugins/datatable/js/buttons.bootstrap5.min.js') }}"></script>
-    <script src="{{ asset('build/assets/plugins/datatable/js/jszip.min.js') }}"></script>
-    <script src="{{ asset('build/assets/plugins/datatable/pdfmake/pdfmake.min.js') }}"></script>
-    <script src="{{ asset('build/assets/plugins/datatable/pdfmake/vfs_fonts.js') }}"></script>
-    <script src="{{ asset('build/assets/plugins/datatable/js/buttons.html5.min.js') }}"></script>
-    <script src="{{ asset('build/assets/plugins/datatable/js/buttons.print.min.js') }}"></script>
-    <script src="{{ asset('build/assets/plugins/datatable/js/buttons.colVis.min.js') }}"></script>
-    <script src="{{ asset('build/assets/plugins/datatable/dataTables.responsive.min.js') }}"></script>
-    <script src="{{ asset('build/assets/plugins/datatable/responsive.bootstrap5.min.js') }}"></script>
-    @vite('resources/assets/js/table-data.js')
-@endsection
