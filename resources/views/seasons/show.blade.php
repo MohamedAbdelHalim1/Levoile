c58000e941916c8caed8f023908ed047a52a8629
Mohamed Abdelhalim <mohamedabdelhalim2804@gmail.com>
2025-06-11 00:38:02 +0300

    Translation33

--- resources/views/seasons/show.blade.php
+++ resources/views/seasons/show.blade.php
@@ -13,7 +13,7 @@
             </div>
             <div class="d-flex mb-3">
                 <label class="form-label text-lg"><strong><b>{{ __('messages.code') }}:</b></strong></label>
-                <p class="ms-3">{{ $season->code ?? '{{ __('messages.N/A') }}' }}</p>
+                <p class="ms-3">{{ $season->code ??  __('messages.N/A')  }}</p>
             </div>
             <a href="{{ route('seasons.index') }}" class="btn btn-secondary">{{ __('messages.back') }}</a>
         </div>
